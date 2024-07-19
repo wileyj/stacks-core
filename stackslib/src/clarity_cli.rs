9591308cf45c646ade12d770da38a4df68bf3e3e
wileyj <2847772+wileyj@users.noreply.github.com>
2024-07-19 12:21:18 -0700

    Update clarity_cli.rs

--- stackslib/src/clarity_cli.rs
+++ stackslib/src/clarity_cli.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// comment
+
 use std::ffi::OsStr;
 use std::io::{Read, Write};
 use std::path::PathBuf;
